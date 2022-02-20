22fa5a95d2664bbef23023c98b45a4d68745fb05
yujong-lee <yujonglee@g.skku.edu>
2022-02-21 00:02:17 +0900

    Add example

---
+++ example/example.js
@@ -0,0 +1,6 @@
+// node index.js string ./example.js
+
+const example = () => {
+    const aa = `123`;
+    console.log(aa);
+}
---
+++ example/generated.ts
@@ -0,0 +1,7 @@
+const examplejs = [
+    `const example = () => {`,
+    `    const aa = \`123\`;`,
+    `    console.log(aa);`,
+    `}`,
+    ``,
+]
